fab53a79f91839b1b0ad8a8bd459e3692ec05105
Seiya Yonezaki <seiya.yone@gmail.com>
2021-03-02 22:51:56 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { join } = require("path");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.static(join(__dirname, "public")));
+
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+app.get("/*", (_, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+process.on("SIGINT", function() {
+  process.exit();
+});
+
+module.exports = app;
+
+// Listen on port 3000
+app.listen(3000, () => console.log("Application running on port 3000"));
